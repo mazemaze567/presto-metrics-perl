9069deb8522c9316fc14041eb09d86fc22b37e4e
sho-takeuchi <sho-takeuchi@fout.jp>
2018-04-27 17:16:22 +0900

    Add env files

---
+++ lib/Presto/Metrics/Client.pm
@@ -0,0 +1,26 @@
+package Presto::Metrics::Client;
+use strict;
+use warnings FATAL => 'all';
+
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Presto::Metrics::Client - Presto metrics library for perl
+
+=head1 SYNOPSIS
+
+    use Presto::Metrics::Client
+
+=head1 DESCRIPTION
+
+xxx
+
+=cut
+
---
+++ t/00_compile.t
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+use Test::More;
+
+use_ok $_ for qw(
+    Presto::Metrics::Client
+);
+
+done_testing();
+
